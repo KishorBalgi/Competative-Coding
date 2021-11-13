31f2e510a9f59143c2854277fb9b02bc62e2919e
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-11-13 16:56:37 +0530

    Created CC Chapter

--- Code-Chef/Test.cpp
+++ Code-Chef/Test.cpp
@@ -11,15 +11,9 @@ int main() {
        syncf;
 	cintie;
 	couttie;
-	int n,i,j,k,cnt=0,min,flag=0;
-	cin>>n;
-	int a[n];
-	for(i=0;i<n;i++){
-		cin>>a[i];
-	}
-	for(i=0;i<n-1;i++){
-		for()
-	}
+	 string s;
+        cin>>s.len()>>endl;
+
 	cout<<cnt+n-1<<"\n";
 	return 0;
 }
