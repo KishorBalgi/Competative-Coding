06375071b78097781caad14d9f4920c42c25ad9d
Kishor Balgi <kishorbalgi@gmail.com>
2023-03-30 00:00:29 +0530

    Updates in two sols

--- LeetCode/Two Pointer/trapping_rain_water.cpp
+++ LeetCode/Two Pointer/trapping_rain_water.cpp
@@ -6,42 +6,28 @@ using namespace std;
 
 class Solution {
 public:
-int trap(vector<int>& height) {
-        int j=1,k=1,n=height.size(),preH=height[0],postH=k,units=0;
-        if(height.size()<3) return 0;
-        while(j!=n-1){
-            if(j>=k){
-                k=findNextHighest(j,height);
-                postH=height[k];
-                if(k==j){
-                    if(height[j]>preH){
-                        preH=height[j];
-                    }
-                    j++;
-                    continue;
-                }
-            }
-            if(preH>height[j] && postH>height[j]){
-                units+=(smallestOf(preH,postH)-height[j]);
-            }
-            if(height[j]>preH){
-                preH=height[j];
-            }
-            j++;
-        }
-        return units;
-    }
-    int findNextHighest(int j,vector<int>& height){
-        int highest=j;
-        for(int i=j+1;i<height.size();i++){
-            if(height[i]>height[highest]){
-                highest=i;
+    int trap(vector<int>& height) {
+        int n=height.size(),ans=0;
+        vector<int> left(n),right(n);
+        calcMax(height,left,right);
+        for(int i=0;i<n;i++){
+            if(min(right[i],left[i])>height[i]){
+                ans+=min(right[i],left[i])-height[i];
             }
         }
-        return highest;
+        return ans;
     }
-    int smallestOf(int i,int j){
-        return i<j?i:j;
+    void calcMax(vector<int>& h,vector<int>& left,vector<int>& right){
+        int n=h.size(),m=-1;
+        for(int i=0;i<n;i++){
+            left[i]=m;
+            m=max(m,h[i]);
+        }
+        m=-1;
+        for(int i=n-1;i>=0;i--){
+            right[i]=m;
+            m=max(m,h[i]);
+        }
     }
 };
 
@@ -51,3 +37,42 @@ int main(){
     cout<<s.trap(height)<<endl;
     return 0;
 }
+
+// This shit didnt work for only 2 test cases -> time limit exceded:
+//    int trap(vector<int>& height) {
+//         int j=1,k=1,n=height.size(),preH=height[0],postH=k,units=0;
+//         if(height.size()<3) return 0;
+//         while(j!=n-1){
+//             if(j>=k){
+//                 k=findNextHighest(j,height);
+//                 postH=height[k];
+//                 if(k==j){
+//                     if(height[j]>preH){
+//                         preH=height[j];
+//                     }
+//                     j++;
+//                     continue;
+//                 }
+//             }
+//             if(preH>height[j] && postH>height[j]){
+//                 units+=(smallestOf(preH,postH)-height[j]);
+//             }
+//             if(height[j]>preH){
+//                 preH=height[j];
+//             }
+//             j++;
+//         }
+//         return units;
+//     }
+//     int findNextHighest(int j,vector<int>& height){
+//         int highest=j;
+//         for(int i=j+1;i<height.size();i++){
+//             if(height[i]>height[highest]){
+//                 highest=i;
+//             }
+//         }
+//         return highest;
+//     }
+//     int smallestOf(int i,int j){
+//         return i<j?i:j;
+//     }
