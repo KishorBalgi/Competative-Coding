83101472af6faf29347d109defef0b211b36474c
Kishor Balgi <kishorbalgi@gmail.com>
2023-08-11 21:18:55 +0530

    Added more tree solutions

--- LeetCode/Trees/left_view_bin_tree.cpp
+++ LeetCode/Trees/left_view_bin_tree.cpp
@@ -1,15 +1,18 @@
-// https://practice.geeksforgeeks.org/problems/reverse-level-order-traversal/1
-
+// https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
+// Tree Node
 struct Node
 {
     int data;
-    struct Node *left;
-    struct Node *right;
+    Node* left;
+    Node* right;
 };
+
+vector<int> leftView(struct Node *root);
+
 // Utility function to create a new Tree Node
 Node* newNode(int val)
 {
@@ -17,78 +20,80 @@ Node* newNode(int val)
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
-    
+
     return temp;
 }
+
+
 // Function to Build Tree
 Node* buildTree(string str)
-{   
+{
     // Corner Case
     if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
     for(string str; iss >> str; )
         ip.push_back(str);
-        
+
+    // for(string i:ip)
+    //     cout<<i<<" ";
+    // cout<<endl;
     // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
     while(!queue.empty() && i < ip.size()) {
-            
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
         if(currVal != "N") {
-                
+
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
         if(i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
         if(currVal != "N") {
-                
+
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
-vector<int> reverseLevelOrder(Node* root);
 
-int main()
-{
-
+int main() {
     int t;
     scanf("%d ",&t);
     while(t--)
@@ -96,48 +101,62 @@ int main()
         string s;
         getline(cin,s);
         Node* root = buildTree(s);
-        vector<int> result = reverseLevelOrder(root);
-        for (int i = 0; i < result.size(); ++i)
-        {
-            cout<<result[i]<<" ";
-        }
-        cout<<endl;
+        vector<int> vec = leftView(root);
+        for(int x : vec)
+        cout<<x<<" ";
+        cout << endl;
     }
-    return 1;
+    return 0;
 }
+
+
 // } Driver Code Ends
 
 
-/*   
+
+
+/* A binary tree node
+
 struct Node
 {
     int data;
-    Node* left;
-    Node* right;
-}; */
-vector<int> reverseLevelOrder(Node *root)
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+//Function to return a list containing elements of left view of the binary tree.
+vector<int> leftView(Node *root)
 {
-    // code here
+   // Your code here
     vector<int> res;
+    if(!root) return res;
     queue<Node*> nodes;
     nodes.push(root);
-    
+    int traversed=0,level=1;
+       
     while(!nodes.empty()){
-        int n=nodes.size();
-        
+        int n = nodes.size();
+           
         while(n--){
-            Node* front=nodes.front();
-            if(front->right){
-                nodes.push(front->right);
+            Node * front=nodes.front();
+               
+            if(front->left) nodes.push(front->left);
+            if(front->right) nodes.push(front->right);
+               
+            if(traversed<level){
+                res.push_back(front->data);
+                traversed++;
             }
-            
-            if(front->left){
-                nodes.push(front->left);
-            }
-            res.insert(res.begin(),front->data);
             nodes.pop();
         }
+        level++;
     }
-    
+       
     return res;
 }
